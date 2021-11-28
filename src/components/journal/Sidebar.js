b6ce9974bfd51f675c68e4ab3ba8ccce78d41570
John <john.davis.2015.03@gmail.com>
2021-11-27 21:50:37 -0500

    implementando scss

---
+++ src/components/journal/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Sidebar = () => {
+	return (
+		<aside className="journal__sidebar">
+			<div className="journal__sidebar-navbar">
+				<h3 className="mt-5">
+					<i className="far fa-moon" />
+					<span>John Davis</span>
+				</h3>
+
+				<button />
+			</div>
+		</aside>
+	);
+};
