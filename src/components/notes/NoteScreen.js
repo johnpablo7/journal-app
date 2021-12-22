e9961d29966e0371f2164d90f1bdbb43f66c70f7
John Pablo Valverde <john.davis.2015.03@gmail.com>
2021-12-21 19:54:33 -0500

    Borrar una nota

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,33 +1,41 @@
 import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { activeNote } from '../../actions/notes';
+import { activeNote, startDeleting } from '../../actions/notes';
 import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
+	const dispatch = useDispatch();
 
-	const dispatch = useDispatch()
-
-	const { active: note } = useSelector(state => state.notes)
-	const [formValues, handleInputChange, reset] = useForm(note)
+	const { active: note } = useSelector((state) => state.notes);
+	const [ formValues, handleInputChange, reset ] = useForm(note);
 	// console.log(formValues);
-	const { body, title } = formValues;
+	const { body, title, id } = formValues;
 
-	const activeId = useRef(note.id)
+	const activeId = useRef(note.id);
 
-	useEffect(() => {
+	useEffect(
+		() => {
+			if (note.id !== activeId.current) {
+				reset(note);
+				activeId.current = note.id;
+			}
+		},
+		[ note, reset ]
+	);
 
-		if (note.id !== activeId.current) {
-			reset(note);
-			activeId.current = note.id
-		}
+	useEffect(
+		() => {
+			// console.log(formValues)
+			dispatch(activeNote(formValues.id, { ...formValues }));
+		},
+		[ formValues, dispatch ]
+	);
 
-	}, [note, reset])
-
-	useEffect(() => {
-		// console.log(formValues)
-		dispatch(activeNote(formValues.id, { ...formValues }));
-	}, [formValues, dispatch]);
+	const handleDelete = () => {
+		// console.log(id);
+		dispatch(startDeleting(id));
+	};
 
 	return (
 		<div className="notes__main-content">
@@ -39,7 +47,7 @@ export const NoteScreen = () => {
 					placeholder="Some awesome title"
 					className="notes__title-input"
 					autoComplete="off"
-					name='title'
+					name="title"
 					value={title}
 					onChange={handleInputChange}
 				/>
@@ -47,23 +55,21 @@ export const NoteScreen = () => {
 				<textarea
 					placeholder="What happened today"
 					className="notes__textarea"
-					name='body'
+					name="body"
 					value={body}
 					onChange={handleInputChange}
 				/>
 
-				{
-					(note.url)
-					&& (
-						<div className="notes__image">
-							<img
-								src="https://images3.alphacoders.com/963/thumb-1920-96395.jpg"
-								alt="sharingan"
-							/>
-						</div>
-					)
-				}
+				{note.url && (
+					<div className="notes__image">
+						<img src={note.url} alt="sharingan" />
+					</div>
+				)}
 			</div>
+
+			<button className="btn btn-danger" onClick={handleDelete}>
+				Delete
+			</button>
 		</div>
 	);
 };
--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { startSaveNote } from '../../actions/notes';
+import { startSaveNote, startUploading } from '../../actions/notes';
 
 export const NotesAppBar = () => {
-
-	const dispatch = useDispatch()
-	const { active } = useSelector(state => state.notes)
+	const dispatch = useDispatch();
+	const { active } = useSelector((state) => state.notes);
 
 	const handleSave = () => {
 		// console.log(active)
 		dispatch(startSaveNote(active));
-	}
+	};
+
+	const handlePictureClick = () => {
+		// console.log('picture');
+		document.querySelector('#fileSelector').click();
+	};
+
+	const handleFileChange = (e) => {
+		// console.log(e.target.files);
+		const file = e.target.files[0];
+		if (file) {
+			dispatch(startUploading(file));
+		}
+	};
 
 	return (
 		<div className="notes__appbar">
 			<span>29 de noviembre del 2022</span>
+			<input
+				id="fileSelector"
+				type="file"
+				name="file"
+				style={{ display: 'none' }}
+				onChange={handleFileChange}
+			/>
 
 			<div>
-				<button className="btn">Picture</button>
+				<button className="btn" onClick={handlePictureClick}>
+					Picture
+				</button>
 
-				<button
-					className="btn"
-					onClick={handleSave}
-				>
+				<button className="btn" onClick={handleSave}>
 					Save
 				</button>
 			</div>
