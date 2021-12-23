e1dde8b54c377aacec653bf2b67cd74934137fb2
john <john.davis.2015.03@gmail.com>
2021-12-23 10:50:56 -0500

    animaciones

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -10,7 +10,7 @@ export const LoginScreen = () => {
 
 	const { loading } = useSelector(state => state.ui)
 
-	const [ formValues, handleInputChange ] = useForm({
+	const [formValues, handleInputChange] = useForm({
 		email: 'theo.03@gmail.com',
 		password: '123456'
 	})
@@ -24,24 +24,27 @@ export const LoginScreen = () => {
 	}
 
 	const handleGoogleLogin = () => {
-		dispatch( startGoogleLogin() );
+		dispatch(startGoogleLogin());
 	}
 
 	return (
 		<>
 			<h3 className="auth__title">Login</h3>
-			<form onSubmit= {handleLogin}>
-				<input type="text" placeholder="Email" name="email" className="auth__input" autoComplete="off" value={ email } onChange={ handleInputChange }/>
-				<input type="password" placeholder="Password" name="password" className="auth__input" value={ password } onChange={ handleInputChange }/>
-				<button type="submit" className="btn btn-primary btn-block" disabled={ loading }>
+			<form
+				onSubmit={handleLogin}
+				className='animate__animated animate__fadeIn animate__faster'
+			>
+				<input type="text" placeholder="Email" name="email" className="auth__input" autoComplete="off" value={email} onChange={handleInputChange} />
+				<input type="password" placeholder="Password" name="password" className="auth__input" value={password} onChange={handleInputChange} />
+				<button type="submit" className="btn btn-primary btn-block" disabled={loading}>
 					Login
 				</button>
 
 				<div className="auth__social-networks">
 					<p>Login with social networks</p>
-					<div 
+					<div
 						className="google-btn"
-						onClick={ handleGoogleLogin }
+						onClick={handleGoogleLogin}
 					>
 						<div className="google-icon-wrapper">
 							<img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -11,17 +11,17 @@ import { startRegisterWithEmailPasswordName } from '../../actions/auth';
 export const RegisterScreen = () => {
 
 	const dispatch = useDispatch();
-	const {msgError} = useSelector(state => state.ui)
+	const { msgError } = useSelector(state => state.ui)
 	// console.log(msgError);
 
-	const [ formValues, handleInputChange ] = useForm({
+	const [formValues, handleInputChange] = useForm({
 		name: 'John',
 		email: 'john.davis.2015.03@gmail.com',
 		password: '123456',
 		password2: '123456',
 	})
 
-	const {name, email, password, password2} = formValues;
+	const { name, email, password, password2 } = formValues;
 
 	const handleRegister = (e) => {
 		e.preventDefault();
@@ -29,31 +29,34 @@ export const RegisterScreen = () => {
 
 		if (isFormValid()) {
 			// console.log('Formulario correcto');
-			dispatch( startRegisterWithEmailPasswordName(email, password, name) );
+			dispatch(startRegisterWithEmailPasswordName(email, password, name));
 		}
 	}
 
 	const isFormValid = () => {
 
-		if (name.trim().length === 0){
-			dispatch( setError('Name is required') )
+		if (name.trim().length === 0) {
+			dispatch(setError('Name is required'))
 			return false;
-		} else if ( !validator.isEmail(email)){
-			dispatch( setError('Email is not valid'))
+		} else if (!validator.isEmail(email)) {
+			dispatch(setError('Email is not valid'))
 			return false;
-		} else if ( password !== password2 || password.length < 5) {
-			dispatch( setError('Password should be at least 6 characters and match each other'))
+		} else if (password !== password2 || password.length < 5) {
+			dispatch(setError('Password should be at least 6 characters and match each other'))
 			return false
 		}
 
-		dispatch( removeError());
+		dispatch(removeError());
 		return true;
 	}
 
 	return (
 		<>
 			<h3 className="auth__title">Register</h3>
-			<form onSubmit={handleRegister}>
+			<form
+				onSubmit={handleRegister}
+				className='animate__animated animate__fadeIn animate__faster'
+			>
 
 				{
 					msgError &&
@@ -62,10 +65,10 @@ export const RegisterScreen = () => {
 					</div>)
 				}
 
-				<input type="text" placeholder="Name" name="name" className="auth__input" autoComplete="off" value={name} onChange={ handleInputChange }/>
-				<input type="text" placeholder="Email" name="email" className="auth__input" autoComplete="off" value={email} onChange={ handleInputChange }/>
-				<input type="password" placeholder="Password" name="password" className="auth__input" value={password} onChange={ handleInputChange }/>
-				<input type="password" placeholder="Confirm password" name="password2" className="auth__input" value={password2} onChange={ handleInputChange }/>
+				<input type="text" placeholder="Name" name="name" className="auth__input" autoComplete="off" value={name} onChange={handleInputChange} />
+				<input type="text" placeholder="Email" name="email" className="auth__input" autoComplete="off" value={email} onChange={handleInputChange} />
+				<input type="password" placeholder="Password" name="password" className="auth__input" value={password} onChange={handleInputChange} />
+				<input type="password" placeholder="Confirm password" name="password2" className="auth__input" value={password2} onChange={handleInputChange} />
 				<button type="submit" className="btn btn-primary btn-block mb-5" >
 					Register
 				</button>
